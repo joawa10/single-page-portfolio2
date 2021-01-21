4309e48b24c6ff7911f62dd54e76f270063380a2
joawa10 <joawa10@yahoo.com>
2021-01-21 21:45:30 +0800

    style contact section & attempt to add validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  getErrorMessage() {
+    if (this.emailFormControl.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.emailFormControl.hasError('email') ? 'Not a valid email' : '';
+  }
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() { }
+
+  onSubmit() {
+
+    console.log("FORM SEND");
+
   }
 
 }
